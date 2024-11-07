38d3c97a65648a8f90abefb5c50e87cbe55a2b5f
Sassan <sassan.ostvar@gmail.com>
2024-11-07 15:00:36 -0500

    set up repo and add record class

---
+++ src/record.py
@@ -0,0 +1,24 @@
+'''
+    Manage wing transmission microscopy data
+'''
+class Record:
+    
+    RECORD_ENTRIES = {
+        "SPECIMEN_ID",
+        "CONTAINER_TYPE",
+        "DATE_COLLECTED",
+        "LOC",
+        "SEX",
+        "PRESERV_MEDIUM",
+        "DRIED",
+        "IMAGING_INSTRUMENT",
+        "DATE_IMAGED",
+        "MAG",
+        "OBJ",
+        "DAMAGED",
+        "SUCCESS",
+        "NOTES",
+    }
+
+    def __init__(self):
+        pass
